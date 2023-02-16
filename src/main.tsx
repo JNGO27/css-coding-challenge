e025e66a106b0dd138b471ba054e9129ee42e55b
Joseph Ochoa <josephochoa91@gmail.com>
2023-02-15 20:15:51 -0800

    Install icons

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { Navbar } from "./components";
 
+import { Navbar } from "./components";
 import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
