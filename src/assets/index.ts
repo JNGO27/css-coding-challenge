36121319d5e259b432370b541b8a82b02f607770
Joseph Ochoa <josephochoa91@gmail.com>
2023-02-17 10:16:28 -0800

    Remove unneeded assets exports

--- src/assets/index.ts
+++
@@ -1,7 +0,0 @@
-import CM_LOGO from "./CM_logo-icon.svg";
-import SearchIcon from "./search-icon.svg";
-import MegaPhoneIcon from "./megaphone.svg";
-import BellIcon from "./bell.svg";
-import HamburgerMenuIcon from "./hamburger-menu.svg";
-
-export { CM_LOGO, SearchIcon, MegaPhoneIcon, BellIcon, HamburgerMenuIcon };
--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -3,6 +3,8 @@ import "./styles.css";
 const Menu = () => {
   const handleMenuLogic = (): void => {
     // Omitted for brevity, but this is where the logic, for onClick go.
+    // eslint-disable-next-line no-console
+    console.log("Menu");
   };
 
   return (
